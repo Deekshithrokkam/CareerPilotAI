5e2f1defab808a7840a4bc8daef970824c450237
Deekshith Rokkam <saideekshithrokkam@gmail.com>
2026-07-22 15:51:04 +0530

    Remove ws transport for Supabase

--- server/src/config/supabase.ts
+++ server/src/config/supabase.ts
@@ -1,13 +1,9 @@
 import { createClient } from "@supabase/supabase-js";
 import { env } from "./env.js";
-import WebSocket from "ws";
 
 const commonOptions = {
   auth: {
     persistSession: false,
-  },
-  realtime: {
-    transport: WebSocket,
   },
 };
 
